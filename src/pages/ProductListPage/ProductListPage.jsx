c155a2241441c8a929d5d457a32645e505fd42e5
aefryyu <imron.rivaille99@gmail.com>
2024-11-24 23:48:35 +0700

    add product list

--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -1,16 +1,27 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
 import Navigation from "../../components/Navigation/Navigation";
 import FilterIcon from "../../components/common/FilterIcon";
 import content from '../../data/content.json'
-import category from "../../components/Sections/Categories/Category";
+import Categories from "../../components/Filter/Categories";
+import PriceFilter from "../../components/Filter/PriceFilter";
+import ColorsFilter from "../../components/Filter/ColorsFilter";
+import SizeFilter from "../../components/Filter/SizeFilter";
+import ProductCard from "./ProductCard";
 
 const categories = content?.categories
 
+
 const ProductListPage = ({categoryType}) => {
+
+    const [products,setProducts] = useState([]);
 
     const categoryContent =  useMemo(() => {
         return categories?.find((category)=> category.code === categoryType );
     }, [categoryType]);
+
+    const productListItems = useMemo( () => {
+        return content?.products?.filter((product)=> product?.category_id === categoryContent?.id);
+    },[categoryContent])
 
     return (
         <div>
@@ -25,13 +36,32 @@ const ProductListPage = ({categoryType}) => {
 
                     <div>
                         <p className='text-[16px] text-black mt-5'>Categories </p>
+                        <Categories types={categoryContent?.types} />
+                        <hr></hr>
+                    </div>
+                    <div>
+                        {/*price*/}
+                        <PriceFilter />
+                        <hr></hr>
+                        {/*colors*/}
+                        <ColorsFilter colors={categoryContent?.meta_data?.colors} />
+                        <hr></hr>
+                        {/*Sizes*/}
+                        <SizeFilter sizes={categoryContent?.meta_data?.sizes} />
                     </div>
                 </div>
 
                 <div className='p-[15px]'>
                     <p className='text-black text-lg'>{categoryContent?.description}</p>
                     {/*product*/}
+                    <div className='pt-4 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8 px-4'>
+                        {productListItems?.map((item,index)=>(
+                            <ProductCard key={index} {...item}/>
+                        ))}
+
+                    </div>
                 </div>
+
             </div>
         </div>
     )
